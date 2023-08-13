6f21ffa29598b94bba856d596f9b76287baa2d71
PrabhatKumardhiman <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-08-13 21:42:50 +0000

    Uploading to GitHub Pages

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -4,7 +4,7 @@ const LoadingSpinner = () => {
     return (
         <div className= 'text-center mt-3'>
             <div className="spinner-border text-primary" role="status">
-                <span className="visually-hidden">Loading...</span>
+                <span className="visually-hidden">Loading....</span>
             </div>
 
         </div>
