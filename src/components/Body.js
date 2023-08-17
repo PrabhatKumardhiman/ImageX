b2009649505e66c897cd99f328b49e2d00886ce0
PrabhatKumardhiman <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-08-17 17:52:00 +0000

    improved Speed

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,7 +29,7 @@ export default class Body1 extends Component {
             totalPage: 0,
             totalImages: 1,
             modal: false,
-            category: "",
+            category: this.props.category,
             message: true,
             value: ""
         }
@@ -46,7 +46,7 @@ export default class Body1 extends Component {
         this.setState({
             loading: true
         })
-        let url = `https://api.unsplash.com/search/photos/?client_id=lF2uW3wfDt5MxaYkJPwLQVwM08hTyFYhjf5JjF0-Qpg&per_page=30&query=${this.state.category ? this.state.category : this.props.category}&page=${this.state.page}`
+        let url = `https://api.unsplash.com/search/photos/?client_id=lF2uW3wfDt5MxaYkJPwLQVwM08hTyFYhjf5JjF0-Qpg&per_page=30&query=${this.state.category}&page=${this.state.page}`
         let data = await fetch(url)
         this.props.setProgress(50)
         let fetchedData = await data.json()
@@ -92,14 +92,6 @@ export default class Body1 extends Component {
                 }} onClick={() => this.setState({ modal: false, currentImgUrl: "" })} />
                 <img effect="blur" src={this.state.currentImgUrl} alt="" />
             </div>
-            <div className="searchContainer"  >
-                <form className="search" style={{ padding: "1rem" }} onSubmit={this.handleForm}>
-                    <p style={{ fontSize: "50px", color: "#493A74", fontWeight: "700" }} >Search the Image you want</p>
-                    <input type="text" onChange={(e) => this.setState({ value: e.target.value })} name="" id="" style={{ width: "100%", height: "50px", padding: "1rem", borderRadius: "10px", border: "none", outline: "none", boxShadow: "0px 0px 10px -1px rgba(0,0,0,0.75)" }} />
-                </form>
-                <img src="https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODI3ODJ8MHwxfHNlYXJjaHwxMnx8Z2VuZXJhbHxlbnwwfHx8fDE2OTIwODc3Njd8MA&ixlib=rb-4.0.3" alt="" style={{ height: "500px", width: "100%", objectFit: "cover" }} />
-            </div>
-
             <div className="container">
                 <InfiniteScroll style={{ overflow: "hidden" }}
                     dataLength={this.state.article.length} //This is important field to render the next data
